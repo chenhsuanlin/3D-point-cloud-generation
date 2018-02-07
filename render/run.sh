52841690ff0dd6740a245d17c7e1a0a3165720a2
Chen-Hsuan Lin <chlin@cmu.edu>
2018-02-07 14:18:57 -0500

    script fix

--- render/run.sh
+++ render/run.sh
@@ -6,4 +6,4 @@ python2 convertEXR.py $1 $1.list 128 $2
 # blender blank.blend -b -P render.py -- /dataset/ShapeNetCore.v2 03001627 03001627.list 128
 # python2 convertEXR.py 03001627 03001627.list 128 8
 
-# ./render_fixed.sh 03001627 8
+# ./run.sh 03001627 8
